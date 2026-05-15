eaddace2df0d3da7e8fba60c5eab55f832b38fec
Alejandro <alemperez24@gmail.com>
2026-05-14 20:05:05 -0400

    Implement extract keywords

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def main():
                 file_path = save_uploaded_file(file)
                 pdf_info = extract_pdf_info(file_path)
                 
-                # TO DO
                 keywords = extract_keywords(file_name, pdf_info)
 
 
@@ -26,24 +25,43 @@ def main():
         return render_template("index.html")
 
 
-    def save_uploaded_file(file):
+    def save_uploaded_file(file) -> str:
         path = f"./uploads/{file.filename}"
         file.save(path)
         
         return path
         
     
-    def extract_pdf_info(saved_path):
+    def extract_pdf_info(saved_path) -> str:
         reader = PdfReader(saved_path)
         page = reader.pages[0]
         text = page.extract_text()
+        print(text)
         return text
 
     
     def extract_keywords(file_name, pdf_info):
-        ...
+        subject_keywords = {
+            "calculus" : {"mathematics", "derivative", "mac", "function"},
+            "composition" : {"essay", "enc", "citation", "paragraph"},
+            "physics" : {"force", "velocity", "phy", "acceleration", "energy"},
+            "programming" : {"python", "cop", "flask", "function", "class"},
+        }
+        
+        combined_text = f"{file_name} {pdf_info or  ' '}".lower()
+        scores = {}
 
-    
+        for subject, keywords in subject_keywords.items():
+            for keyword in keywords:
+                if keyword in combined_text:
+                    scores[subject] = scores.get(subject, 0) + 1
+
+        if not scores:
+            return "unknown"
+        
+        return max(scores, key=scores.get)
+            
+
     app.run(debug=True)
 if __name__ == "__main__":
     main()
